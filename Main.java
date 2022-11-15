0fac249787765467e9cceb7de52b1ca1fda81e9c
arsyikamarifatika <93310658+arsyikamarifatika@users.noreply.github.com>
2022-11-15 18:04:06 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lingkaran;
+
+import com.lingkaran.*;
+public class Main {
+    public static void main(String[] args) {
+        Lingkaran x = new SetengahLingkaran();
+        LuasSetengah y = new LuasSetengah(x);
+        
+        
+        
+        
+    }
+    
+}
---
+++ SetengahLingkaran.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lingkaran;
+
+/**
+ *
+ * @author Yulinar sri wahyuni
+ */
+public class SetengahLingkaran implements Lingkaran {
+    double hasil;
+    public SetengahLingkaran(){
+        hasil = 0;
+    }
+    @Override
+    public void hitungLuas() {
+        hasil = (panjang * lebar) * (Math.PI * lebar * lebar);
+    }
+    
+    public double getHasil(){
+        return hasil;
+    }
+}
